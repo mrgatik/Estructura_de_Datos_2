37d84fc728b6dcfcdc160191ff2ad4ae5ac09b71
mrgatik <39605786+mrgatik@users.noreply.github.com>
2019-08-23 00:31:05 -0500

    Create s4-tarea2.py

---
+++ s4-tarea2.py
@@ -0,0 +1,21 @@
+#arbol binario valor de cada nodo
+import os 
+import random 
+ 
+a = 50 
+b = 40 
+c = 60 
+d = 25 
+e = 45 
+f = 55 
+g = 65 
+  
+print (random (3 - 14)) 
+  if ((a>b) and (a>c) and (a>d) and (a>e) and (a>f) and (a>g) and (a>h) and (a>i))
+  print (a) 
+    if ((b>d) and (b>e)) and (b<a)):
+    print (b) 
+      if ((c>f) and (c<g) and (c>a)):
+      print (c) 
+        if ((d<b) and (d<a) and (d<e)):
+        print (d) 
