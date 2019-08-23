ca8b776dca1fa03c412e9bb148ecbabc9b1984ed
mrgatik <39605786+mrgatik@users.noreply.github.com>
2019-08-23 00:22:41 -0500

    Create s2-tarea1.py

---
+++ s2-tarea1.py
@@ -0,0 +1,22 @@
+a = int(input("introducir la raiz:"))
+b = 40
+c = 60
+d = 25
+e = 45
+f = 55
+g = 65
+
+if (a!= None ):
+  print(a)
+  if((b>d) and (e>b) and (b<a)):
+    print(b)
+    if ((d<b) and (d<e) and (d>a)):
+      print (d)
+      if((e>b) and (e<a) and (e>d)):
+        print (e)
+        if((c>a) and (c>b)):
+          print(c)
+          if((c>f) and (f>a) and (f<g)):
+            print (f)
+            if((g>c) and (g>a)):
+              print(g)
